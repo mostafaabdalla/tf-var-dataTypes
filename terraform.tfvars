d24a28f4d65379253e882e66736586b38bb5c00f
mostafa <mostafaa.abdallaa@gmail.com>
2022-01-18 13:40:50 +0000

    Added project files

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+usernumber = "1234"
+elb_name   = "myelb"
+az         = ["us-east-1a", "us-east-1b"]
+timeout    = "400"
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "usernumber" {
+  type = number
+}
+
+variable "elb_name" {
+  type = string
+}
+
+variable "az" {
+  type = list(any)
+}
+
+
+variable "timeout" {
+  type = number
+}
